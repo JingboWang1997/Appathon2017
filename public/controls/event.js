5f94049095978282272ee76c0f7c1d3e3a103bdf
John Lin <bigjohnlin@gmail.com>
2017-10-29 09:32:43 -0400

    Auto stash before merge of "master" and "origin/master"

--- public/controls/event.js
+++ public/controls/event.js
@@ -2,54 +2,54 @@ var localData;
 
 $(document).ready(function() {
     $(".searchButton").click(function() {
-        console.log("first test");
-        $.get('https://appathon2017.azurewebsites.net/resources/', {}, function(data){
+        //console.log("first test"); //appathon2017.azurewebsites.net
+        $.get('http://127.0.0.1:3000/resources/', {}, function(data){
         	localData = data;
-        	console.log("test"); 
-            if (typeof(data.video) == "undefined" ) { 
-                console.log('rerun, undefined'); 
-            } else { 
-                //console.log(data.video[0]); 
-                for (var a = 0; a < data.video.length; a++) { 
-                    var id = "#video" + a; 
-                    $(id).attr("src",data.video[a]); 
-                } 
+            if (typeof(data.video) == "undefined" ) {
+                console.log('rerun, undefined');
+            } else {
+                //console.log(data.video[0]);
+                for (var a = 1; a < data.video.length + 1; a++) {
+                    var id = "#video" + a;
+                    $(id).attr("src",data.video[a-1]);
+                }
+                for (var b = 1; b < 17; b++) {
+                    var id1 = "#img" + b;
+                    $(id1).attr("src", data.image[b-1]);
+                }
             }
-            // console.log(data);
-            // console.log("test");
-            //console.log(data.video.length);
-            //src = 'https://www.youtube.com/embed/' + id;
+
         });
     });
 
 });
 
-function updateImage() {
-
-    //div.innerHTML = '<img class="cover" src="https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi" alt="image" style="width:100%">';
-
-    var mydiv = document.getElementById('Images');
-	while(mydiv.firstChild) {
-	  	mydiv.removeChild(mydiv.firstChild);
-	}
-	if (localData != undefined) {
-		console.log("data: " + localData.image.length);
-		for (var i = 0; i < localData.image.length; i++) {
-
-			var div = document.createElement("div");
-			var img = document.createElement("img");
-			div.className = 'floating-box';
-			img.src = localData.image[i].toString();
-			img.className = "cover";
-
-
-			div.appendChild(img);
-	    	
-	    	console.log(localData.image[i]);
-			
-			mydiv.appendChild(div);
-		}
-	} else {
-		console.log("not working");
-	}
-}
+// function updateImage() {
+//
+//     //div.innerHTML = '<img class="cover" src="https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi" alt="image" style="width:100%">';
+//
+//     var mydiv = document.getElementById('Images');
+// 	while(mydiv.firstChild) {
+// 	  	mydiv.removeChild(mydiv.firstChild);
+// 	}
+// 	if (localData != undefined) {
+// 		console.log("data: " + localData.image.length);
+// 		for (var i = 0; i < localData.image.length; i++) {
+//
+// 			var div = document.createElement("div");
+// 			var img = document.createElement("img");
+// 			div.className = 'floating-box';
+// 			img.src = localData.image[i].toString();
+// 			img.className = "cover";
+//
+//
+// 			div.appendChild(img);
+//
+// 	    	console.log(localData.image[i]);
+//
+// 			mydiv.appendChild(div);
+// 		}
+// 	} else {
+// 		console.log("not working");
+// 	}
+// }
